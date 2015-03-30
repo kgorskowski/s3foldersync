84561d4c032e33a83c263c6539adca4bab9827b8
Karsten Gorskowski <kgorskowski@codemonauts.com>
2015-03-30 22:43:49 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
-LOGFILE='var/log/s3sync.log'
+TIMESTAMP=$(date +%Y:%m:%d:%H:%S)
+LOGFILE='var/log/s3sync_$TIMESTAMP.log'
 logg () {
-        echo -n `date +%Y%m%d%H%S` >> $LOGFILE
         echo "`basename $0`$1" >> $LOGFILE
 }
 if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$S3_SOURCE" ] || [ -z "$S3_DESTINATION" ] || [ -z "$SRC_REGION" ] || [ -z "$DEST_REGION" ] ; then
@@ -9,8 +9,6 @@ if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$S3_S
   exit 1
 fi
 logg " started"
-echo "Content of Source" $(aws s3 ls s3://$S3_SOURCE --human-readable --region $SRC_REGION) >> $LOGFILE
-echo "Content of Destination" $(aws s3 ls s3://$S3_DESTINATION --human-readable --region $DEST_REGION) >> $LOGFILE
 
 do_sync () {
         aws s3 sync --delete s3://$S3_SOURCE s3://$S3_DESTINATION --delete --source-region $SRC_REGION --region $DEST_REGION
