88f44e846ffdb3a0aa3a622b3905c0b34541999d
Karsten Gorskowski <kgorskowski@codemonauts.com>
2015-03-30 23:09:27 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$S3_S
   echo "Please set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_SOURCE Bucket and SRC_REGION and S3_DESTINATION Bucket and DEST_REGION in env vars" 1>&2
   exit 1
 fi
-logg "  started at `date +%Y:%m:%d:%H:%S`"
+logg "  started at `date +%Y%m%d_%H:%M:%S`"
 
 do_sync () {
         aws s3 sync --delete s3://$S3_SOURCE s3://$S3_DESTINATION --delete --source-region $SRC_REGION --region $DEST_REGION
@@ -25,4 +25,4 @@ else
     sleep $(( $INTERVAL - (`date +'%s'` - $s) ))
   done
 fi
-logg " ended at `date +%Y:%m:%d:%H:%S`"
+logg " ended at `date +%Y%m%d_%H:%M:%S`"
