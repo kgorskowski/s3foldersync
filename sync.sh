aa7c39614815758d17577f7a74eb27a8a7460160
kgorskowski <kgorskowski@codemonauts.com>
2015-03-31 23:21:47 +0200

    add do_delete option in sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -e
-
 LOGFILE=var/log/s3sync.log
 logg () {
         echo "`basename $0`$1"
@@ -8,22 +7,16 @@ if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$S3_S
   echo "Please set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_SOURCE Bucket and SRC_REGION and S3_DESTINATION Bucket and DEST_REGION in env vars" 1>&2
   exit 1
 fi
-logg "  started at `date +%Y%m%d_%H:%M:%S`"
+logg " started at `date +%Y%m%d_%H:%M:%S`"
+
 
 do_sync () {
         aws s3 sync s3://$S3_SOURCE s3://$S3_DESTINATION --source-region $SRC_REGION --region $DEST_REGION
 }
-if [ -z "$INTERVAL" ]; then
+if [ "$DO_DELETE" = true ]; then
   # Run once
-  do_sync
+  do_sync --delete
 else
-  # Loop every $INTERVAL seconds
-  while true; do
-    s=`date +'%s'`
-
-    do_sync
-
-    sleep $(( $INTERVAL - (`date +'%s'` - $s) ))
-  done
+  do_sync
+logg " backup ended at `date +%Y%m%d_%H:%M:%S`"
 fi
-logg " ended at `date +%Y%m%d_%H:%M:%S`"
